db8d1e8ce6381c6468cb52dc9f811bda5956154a
Pao <Pao@DESKTOP-EASQQC0>
2022-03-09 00:45:07 -0300

    Eliminados los métodos obterX, obterY, obterRadio, establecerX, establecerY y establecerRadio.

--- src/main/java/circulo/Aro.java
+++ src/main/java/circulo/Aro.java
@@ -45,7 +45,11 @@ public class Aro {
      * @param radio the radio to set
      */
     public void setRadio(double radio) {
+        if(radio < 0){
+            this.radio = 0;
+        }else{
         this.radio = radio;
+        }
     }
     public static final double MINIMO = 0.0;
     public static final double LIMITERADIO = 0.0;
@@ -60,33 +64,14 @@ public class Aro {
     public Aro(int valorX, int valorY, double valorRadio) {
         coordenadaX = valorX;
         coordenadaY = valorY;
-        establecerRadio(valorRadio);
+        setRadio(valorRadio);
     }
 
-    public void establecerX(int valorX) {
-        setCoordenadaX(valorX);
-    }
 
-    public int obterX() {
-        return getCoordenadaX();
-    }
 
-    public void establecerY(int valorY) {
-        setCoordenadaY(valorY);
-    }
 
-    public int obterY() {
-        return getCoordenadaY();
-    }
 
-    public void establecerRadio(double valorRadio) {
 
-        setRadio(valorRadio < MINIMO ? MINIMO : valorRadio);
-    }
-
-    public double obterRadio() {
-        return getRadio();
-    }
 
     public double obterDiametro() {
         return getRadio() * 2;
